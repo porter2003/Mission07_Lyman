52a1a726e00ce27884a51a4bd04acc8eecd4fa5a
porter2003 <156115900+porter2003@users.noreply.github.com>
2025-02-19 23:23:57 -0700

    Lots of changes

--- Mission06_Lyman/Controllers/HomeController.cs
+++ Mission06_Lyman/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Mission06_Lyman.Models;
 using SQLitePCL;
 
@@ -24,22 +25,64 @@ namespace Mission06_Lyman.Controllers
         public IActionResult EnterMovie()
         {
             ViewBag.Categories = _context.Categories.ToList(); // Send categories to view
-            return View();
+            return View("EnterMovie", new Movie());
         }
 
         [HttpPost]
         public IActionResult EnterMovie(Movie response)
         {
-            _context.Movies.Add(response); // Add record to the database
-            _context.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); // Add record to the database
+                _context.SaveChanges();
 
-            return View("Confirmation");
+                return View("Confirmation");
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories.ToList(); // Send categories to view
+                    
+                return View(response);
+            }
         }
         public IActionResult WaitList()
         {
             var movies = _context.Movies.ToList();
 
             return View(movies); 
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)   
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            ViewBag.Categories = _context.Categories.ToList();
+            return View("EnterMovie", recordToEdit);
+        }
+        [HttpPost]
+        public IActionResult Edit(Movie updatedInfo)
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges();
+             
+            return RedirectToAction("WaitList");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View(recordToDelete);
+        }
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("Waitlist");
         }
     }
 }
--- Mission06_Lyman/Models/Movie.cs
+++ Mission06_Lyman/Models/Movie.cs
@@ -4,18 +4,20 @@ namespace Mission06_Lyman.Models
 {
     public class Movie
     {
-        [Required]
+        [Required ]
         public int MovieId{ get; set; }
-        [Required]
-        public int CategoryId { get; set; }
-        [Required]
-        public required string Title { get; set; }
+        [Required (ErrorMessage = "Select a category.")]
+        public int? CategoryId { get; set; }
+        [Required (ErrorMessage = "Enter a title.")]
+        public string? Title { get; set; }
+        [Required (ErrorMessage = "Enter a year after 1888.")]
+        [Range(1888, 2030)]
         public int Year { get; set; }
         public string? Director { get; set; }
         public string? Rating { get; set; }
         public bool Edited { get; set; }
         public string? LentTo { get; set; }
-
+            
         public bool CopiedToPlex { get; set; }
         public string? Notes { get; set; }
 
